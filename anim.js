42d70e193c00779fa9bed0dc311252d973acbbbc
hamza8bit <hamzabyte@gmail.com>
2022-07-27 14:11:52 +0500

    test

---
+++ anim.js
@@ -0,0 +1,18 @@
+function NavExpand(){
+        nav  = document.getElementsByTagName("nav")[0];
+        bars = document.getElementsByClassName("fa-bars")[0];
+        if(nav.style.height=="120px")
+        {
+            nav.removeAttribute("style");
+            bars.removeAttribute("style");
+            nav.style.animationName = "navunexpand"
+            bars.style.animationName= "barsback";
+        }
+        else
+        {
+            nav.style.height="120px";
+            nav.style.animationName = "navexpand"
+            bars.style.color="#F687F7";
+            bars.style.animationName= "bars";
+        }
+    }
